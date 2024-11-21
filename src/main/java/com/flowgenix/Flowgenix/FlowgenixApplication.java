3cd0b20a6fcb02e64b13c2757252c8498d5ca0ba
BytecodeBaron <bartlomiejsarata@gmail.com>
2024-11-21 22:04:33 +0100

    [TECH] Initial commit - adding Spring

---
+++ src/main/java/com/flowgenix/Flowgenix/FlowgenixApplication.java
@@ -0,0 +1,13 @@
+package com.flowgenix.Flowgenix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowgenixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowgenixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flowgenix/Flowgenix/FlowgenixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowgenix.Flowgenix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowgenixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
